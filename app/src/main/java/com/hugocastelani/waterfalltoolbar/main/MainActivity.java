7d2c98e1734d521fbdaa61bba8b90331d1f32b2b
HugoCastelani <HugoCastelaniBP@gmail.com>
2017-12-23 15:42:58 -0200

    Add GitHub menu entry

--- app/src/main/java/com/hugocastelani/waterfalltoolbar/main/MainActivity.java
+++ app/src/main/java/com/hugocastelani/waterfalltoolbar/main/MainActivity.java
@@ -1,9 +1,12 @@
 package com.hugocastelani.waterfalltoolbar.main;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Button;
 
 import com.hugocastelani.waterfalltoolbar.R;
@@ -28,23 +31,23 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         initViews();
-        prepareViews();
+        setupViews();
     }
 
     private void initViews() {
-        mToolbar = findViewById(R.id.toolbar);
-        mInitialElevationSeekBar = findViewById(R.id.initial_elevation_seek_bar);
-        mFinalElevationSeekBar = findViewById(R.id.final_elevation_seek_bar);
-        mScrollFinalPositionSeekBar = findViewById(R.id.scroll_final_position_seek_bar);
-        mDoneButton = findViewById(R.id.done_button);
-        mRestoreDefaultButton = findViewById(R.id.restore_default_button);
+        mToolbar = findViewById(R.id.am_toolbar);
+        mInitialElevationSeekBar = findViewById(R.id.am_initial_elevation_seek_bar);
+        mFinalElevationSeekBar = findViewById(R.id.am_final_elevation_seek_bar);
+        mScrollFinalPositionSeekBar = findViewById(R.id.am_scroll_final_position_seek_bar);
+        mDoneButton = findViewById(R.id.am_done_button);
+        mRestoreDefaultButton = findViewById(R.id.am_restore_default_button);
     }
 
-    private void prepareViews() {
-        // prepare toolbar
+    private void setupViews() {
+        // setup toolbar
         setSupportActionBar(mToolbar);
 
-        // prepare initial elevation seek bar
+        // setup initial elevation seek bar
         mInitialElevationSeekBar.setOnProgressChangeListener(new DiscreteSeekBar.OnProgressChangeListener() {
             @Override public void onProgressChanged(DiscreteSeekBar seekBar, int value, boolean fromUser) {}
             @Override public void onStartTrackingTouch(DiscreteSeekBar seekBar) {}
@@ -58,7 +61,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        // prepare restore default button
+        // setup restore default button
         mRestoreDefaultButton.setOnClickListener(view -> {
             mFinalElevationSeekBar.setMin(0);
             mFinalElevationSeekBar.setMax(10);
@@ -71,7 +74,7 @@ public class MainActivity extends AppCompatActivity {
             mScrollFinalPositionSeekBar.setProgress(WaterfallToolbar.DEFAULT_SCROLL_FINAL_ELEVATION);
         });
 
-        // prepare done button
+        // setup done button
         mDoneButton.setOnClickListener(view -> {
             final Bundle bundle = new Bundle();
             bundle.putInt("initial_elevation", mInitialElevationSeekBar.getProgress());
@@ -83,5 +86,22 @@ public class MainActivity extends AppCompatActivity {
 
             startActivity(intent);
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.see_on_github) {
+            Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/HugoCastelani/waterfall-toolbar"));
+            startActivity(browserIntent);
+            return true;
+        } else {
+            return super.onOptionsItemSelected(item);
+        }
     }
 }
