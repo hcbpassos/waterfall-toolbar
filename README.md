9bb0a5419aced34cd1db0c5b977a42db0e17a2e5
hugo <hugocastelanibp@gmail.com>
2018-02-05 00:21:58 -0200

    Convert WaterfallToolbar to Kotlin Also update documentation.

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 ## Waterfall Toolbar
 [![Android Arsenal](https://img.shields.io/badge/Android%20Arsenal-Waterfall%20Toolbar-brightgreen.svg)](https://android-arsenal.com/details/1/6232)
 [![JitPack](https://jitpack.io/v/hugocastelani/waterfall-toolbar.svg)](https://jitpack.io/#hugocastelani/waterfall-toolbar)<br>
-Waterfall Toolbar is an Android version of Material Design's web component waterfall toolbar. Basically, what it does is dynamize an ordinary Toolbar, increasing and decreasing its shadow when an associated view is scrolled.<br>
+Waterfall Toolbar is an Android version of Material Design's web component waterfall toolbar made in Kotlin. Basically, what it does is dynamize an ordinary Toolbar, increasing and decreasing its shadow when an associated view is scrolled.<br>
 You can download the <a href="https://raw.githubusercontent.com/hugocastelani/waterfall-toolbar/master/sample.apk">sample.apk</a> to get a better notion of what's going on.<br><br>
 <img src="/.github/sample.gif" alt="Sample" width="270"/>
 
@@ -24,12 +24,13 @@ And this one to your module `build.gradle` file:
 ``` gradle
 dependencies {
     ...
-    implementation 'com.github.hugocastelani:waterfall-toolbar:0.3.3'
+    implementation 'com.github.hugocastelani:waterfall-toolbar:0.4.0'
 }
 ```
 
 ## Implementation
-Implementing Waterfall Toolbar is quite simple. All you gotta do is add it to your layout via XML or Java and refer a RecyclerView or a ScrollView.<br>
+Since almost everyone who knows Java knows Kotlin, I'm going to base the implementation in Java, but Kotlin is fully supported.<br>
+Implementing Waterfall Toolbar is quite simple. All you gotta do is add it to your layout and refer a RecyclerView or a ScrollView.<br>
 Your XML code:
 ```xml
 <com.hugocastelani.waterfalltoolbar.WaterfallToolbar
@@ -48,15 +49,13 @@ Your XML code:
 Your Java code:
 ```java
 public class MainActivity extends AppCompatActivity {
- 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         ...
         WaterfallToolbar waterfallToolbar = (WaterfallToolbar) findViewById(R.id.waterfall_toolbar);
-        waterfallToolbar.addRecyclerView(recyclerView);
+        waterfallToolbar.setRecyclerView(recyclerView);
     }
-    
 }
 ```
 
@@ -64,13 +63,29 @@ Congratulations, you're all set!<br>
 Note: you can do whatever you want with your inner Toolbar, Waterfall Toolbar won't interfere.
 
 ## Customization
-Well, there are people who follow standards and people who create their standards. These last ones can customize three aspects of Waterfall Toolbar with Java or XML.<br>
-Note: sample project provides a nice environment to test all these things. Maybe you should give it a try.  
+Note: sample project provides a nice environment to test all these things. Maybe you should give it a try.<br>
+Well, there are people who follow standards and people who create their standards. These last ones can customize three aspects of Waterfall Toolbar with Java or XML.
+However, to set those attributes in Java, you have to use WaterfallToolbar's unit classes: Px and Dp.
+
+### How to use Px and Dp classes
+I've created these classes to simplify the conversion between px and dp, which happens often inside WaterfallToolbar class.
+Using them is quite simple. If you have a WaterfallToolbar object instantiated in your class, just create a new object passing the intended size:
+``` java
+final Pixel pixel = new Pixel(50);
+```
+But if you're using them before instantiating, you have to give your display density:
+``` java
+DimensionUnitsKt.setDensity(getResources().getDisplayMetrics().density);
+```
 
 ### Initial elevation
-The elevation with which the toolbar starts. Default value: 0 dp.<br>
-Through Java: `waterfallToolbar.setInitialElevationDp(1);`<br>
-Through XML: `app:initial_elevation="1dp"`
+The elevation with which the toolbar starts. Default value: 0 dp. Java and XML implementation, respectively:<br>
+``` java
+waterfallToolbar.setInitialElevation(new Dp(1).toPx());
+```
+``` xml
+app:initial_elevation="1dp"
+```
 
 Result:<br><br>
 <img src="/.github/initial.gif" alt="New initial shadow" width="270"/>
@@ -79,9 +94,13 @@ As you can see, there's now a tiny initial shadow.<br>
 Note: Waterfall Toolbar extends CardView, and its elevation in taken seriously by Android. Since elevation is set to 0dp, if there's another view below it, Waterfall Toolbar is going to be overlaid. Fortunately, if you set the views' limits properly, you won't have any related trouble.  
 
 ### Final elevation
-The elevation the toolbar gets when it reaches final scroll elevation. Default value: 4 dp.<br>
-Through Java: `waterfallToolbar.setFinalElevationDp(10);`<br>
-Through XML: `app:final_elevation="10dp"`
+The elevation the toolbar gets when it reaches final scroll elevation. Default value: 4 dp. Java and XML implementation, respectively:<br>
+``` java
+waterfallToolbar.setFinalElevation(new Dp(10).toPx());
+```
+``` xml
+app:final_elevation="10dp"
+```
 
 Result:<br><br>
 <img src="/.github/final.gif" alt="New final shadow" width="270"/>
@@ -89,14 +108,22 @@ Result:<br><br>
 As result, the final shadow gets much bigger.
 
 ### Scroll final position
-The percentage of the screen's height that is going to be scrolled to reach the final elevation. Default value: 12%.<br>
+The percentage of the screen's height that is going to be scrolled to reach the final elevation. Default value: 12%. Java and XML implementation, respectively:<br>
 Short value:<br>
-Through Java: `waterfallToolbar.setScrollFinalPosition(4);`<br>
-Through XML: `app:scroll_final_elevation="4"`
+``` java
+waterfallToolbar.setScrollFinalPosition(4);
+```
+``` xml
+app:scroll_final_elevation="4"
+```
 
 Long value:<br>
-Through Java: `waterfallToolbar.setScrollFinalPosition(40);`<br>
-Through XML: `app:scroll_final_elevation="40"`
+``` java
+waterfallToolbar.setScrollFinalPosition(40);
+```
+``` xml
+app:scroll_final_elevation="40"
+```
 
 Respective results:<br><br>
 <img src="/.github/short.gif" alt="Short value to scroll final position" width="270"/>
---
+++ library/src/main/java/com/hugocastelani/waterfalltoolbar/WaterfallToolbar.kt
@@ -0,0 +1,288 @@
+package com.hugocastelani.waterfalltoolbar
+
+import android.content.Context
+import android.os.Build
+import android.os.Parcel
+import android.os.Parcelable
+import android.support.annotation.RequiresApi
+import android.support.v7.widget.CardView
+import android.support.v7.widget.RecyclerView
+import android.util.AttributeSet
+import android.view.View
+import android.widget.ScrollView
+
+/**
+ * Created by Hugo Castelani
+ * Date: 19/09/17
+ * Time: 19:30
+ */
+
+open class WaterfallToolbar : CardView {
+    init {
+        // set density to be able to use DimensionUnits
+        // this code must run before all the signings using DimensionUnits
+        if (density == null) density = resources.displayMetrics.density
+    }
+
+    /**
+     * The recycler view whose scroll is going to be listened
+     */
+    var recyclerView: RecyclerView? = null
+        set(value) {
+            field = value
+            addRecyclerViewScrollListener()
+        }
+
+    /**
+     * The scroll view whose scroll is going to be listened
+     */
+    var scrollView: ScrollView? = null
+        set(value) {
+            field = value
+            addScrollViewScrollListener()
+        }
+
+    /**
+     * The three variables ahead are null safe, since they are always set
+     * at least once in init() and a null value can't be assigned to them
+     * after that. So all the "!!" involving them below are fully harmless.
+     */
+
+    /**
+     * The elevation with which the toolbar starts
+     */
+    var initialElevation: Px? = null
+        set(value) {
+            if (value != null) {
+                field = value
+
+                // got to update elevation in case this value have
+                // been set in a running and visible activity
+                if (isSetup) adjustCardElevation()
+
+            } else throw NullPointerException("This field cannot be null.")
+        }
+
+    /**
+     * The elevation the toolbar gets when it reaches final scroll elevation
+     */
+    var finalElevation: Px? = null
+        set(value) {
+            if (value != null) {
+                field = value
+
+                // got to update elevation in case this value have
+                // been set in a running and visible activity
+                if (isSetup) adjustCardElevation()
+
+            } else throw NullPointerException("This field cannot be null.")
+        }
+
+    /**
+     * The percentage of the screen's height that is
+     * going to be scrolled to reach the final elevation
+     */
+    var scrollFinalPosition: Int? = null
+        set(value) {
+            if (value != null) {
+                val screenHeight = resources.displayMetrics.heightPixels
+                field = Math.round(screenHeight * (value / 100.0f))
+
+                // got to update elevation in case this value have
+                // been set in a running and visible activity
+                if (isSetup) adjustCardElevation()
+
+            } else throw NullPointerException("This field cannot be null.")
+        }
+
+    /**
+     * Dimension units (dp and pixel) auxiliary
+     */
+
+
+    /**
+     * Values related to Waterfall Toolbar behavior in their default forms
+     */
+    val defaultInitialElevation = Dp(0f).toPx()
+    val defaultFinalElevation = Dp(4f).toPx()
+    val defaultScrollFinalElevation = 12
+
+    /**
+     * Auxiliary that indicates if the view is already setup
+     */
+    private var isSetup: Boolean = false
+
+    /**
+     * Position in which toolbar must be to reach expected shadow
+     */
+    private var orthodoxPosition = Px(0)
+
+    /**
+     * Recycler/scroll view real position
+     */
+    private var realPosition = Px(0)
+
+    constructor(context: Context) : super(context) {
+        init(context, null)
+    }
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+        init(context, attrs)
+    }
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int?)
+            : super(context, attrs, defStyleAttr!!) {
+        init(context, attrs)
+    }
+
+    private fun init(context: Context?, attrs: AttributeSet?) {
+        // leave card corners square
+        radius = 0f
+
+        if (context != null && attrs != null) {
+            val typedArray = context.obtainStyledAttributes(attrs, R.styleable.WaterfallToolbar)
+
+            val rawInitialElevation = typedArray.getDimensionPixelSize(
+                    R.styleable.WaterfallToolbar_initial_elevation, defaultInitialElevation.value)
+
+            val rawFinalElevation = typedArray.getDimensionPixelSize(
+                    R.styleable.WaterfallToolbar_final_elevation, defaultFinalElevation.value)
+
+            scrollFinalPosition = typedArray.getInteger(
+                    R.styleable.WaterfallToolbar_scroll_final_elevation, defaultScrollFinalElevation)
+
+            this.initialElevation = Px(rawInitialElevation)
+            this.finalElevation = Px(rawFinalElevation)
+
+            typedArray.recycle()
+
+        } else {
+
+            initialElevation = defaultInitialElevation
+            finalElevation = defaultFinalElevation
+            scrollFinalPosition = defaultScrollFinalElevation
+        }
+
+        adjustCardElevation()    // just to make sure card elevation is set
+
+        isSetup = true
+    }
+
+    private fun addRecyclerViewScrollListener() {
+        recyclerView?.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrolled(recyclerView: RecyclerView?, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+                // real position must always get updated
+                realPosition.value = realPosition.value + dy
+                mutualScrollListenerAction()
+            }
+        })
+    }
+
+    private fun addScrollViewScrollListener() {
+        scrollView?.viewTreeObserver?.addOnScrollChangedListener {
+            // real position must always get updated
+            realPosition.value = scrollView!!.scrollY
+            mutualScrollListenerAction()
+        }
+    }
+
+    /**
+     * These lines are common in both scroll listeners, so they are better joined
+     */
+    private fun mutualScrollListenerAction() {
+        // orthodoxPosition can't be higher than scrollFinalPosition because
+        // the last one holds the position in which shadow reaches ideal size
+
+        if (realPosition.value <= scrollFinalPosition!!) {
+            orthodoxPosition.value = realPosition.value
+        } else {
+            orthodoxPosition.value = scrollFinalPosition!!
+        }
+
+        adjustCardElevation()
+    }
+
+    /**
+     * Speed up the card elevation setting
+     */
+    private fun adjustCardElevation() {
+        cardElevation = calculateElevation().value.toFloat()
+    }
+
+    /**
+     * Calculates the elevation based on given attributes and scroll
+     * @return New calculated elevation
+     */
+    private fun calculateElevation(): Px {
+        // getting back to rule of three:
+        // finalElevation = scrollFinalPosition
+        // newElevation   = orthodoxPosition
+        var newElevation: Int = finalElevation!!.value * orthodoxPosition.value / scrollFinalPosition!!
+
+        // avoid values under minimum value
+        if (newElevation < initialElevation!!.value) newElevation = initialElevation!!.value
+
+        return Px(newElevation)
+    }
+
+    /**
+     * Saves the view's current dynamic state in a parcelable object
+     * @return A parcelable with the saved data
+     */
+    override fun onSaveInstanceState(): Parcelable? {
+        val savedState = SavedState(super.onSaveInstanceState())
+
+        savedState.elevation = cardElevation.toInt()
+        savedState.orthodoxPosition = orthodoxPosition
+        savedState.realPosition = realPosition
+
+        return savedState
+    }
+
+    /**
+     * Restore the view's dynamic state
+     * @param state The frozen state that had previously been returned by onSaveInstanceState()
+     */
+    override fun onRestoreInstanceState(state: Parcelable) {
+        val savedState = state as SavedState
+        super.onRestoreInstanceState(savedState.superState)
+
+        // setCardElevation() doesn't work until view is created
+        post {
+            // it's safe to use "!!" here, since savedState will
+            // always store values properly set in onSaveInstanceState()
+            cardElevation = savedState.elevation!!.toFloat()
+            orthodoxPosition = savedState.orthodoxPosition!!
+            realPosition = savedState.realPosition!!
+        }
+    }
+
+    /**
+     * Custom parcelable to store this view's dynamic state
+     */
+    private class SavedState : View.BaseSavedState {
+        var elevation: Int? = null
+        var orthodoxPosition: Px? = null
+        var realPosition: Px? = null
+
+        internal constructor(source: Parcel) : super(source)
+
+        @RequiresApi(api = Build.VERSION_CODES.N)
+        internal constructor(source: Parcel, loader: ClassLoader) : super(source, loader)
+
+        internal constructor(superState: Parcelable) : super(superState)
+
+        companion object {
+            internal val CREATOR: Parcelable.Creator<SavedState> = object : Parcelable.Creator<SavedState> {
+                override fun createFromParcel(source: Parcel): SavedState {
+                    return SavedState(source)
+                }
+
+                override fun newArray(size: Int): Array<SavedState?> {
+                    return arrayOfNulls(size)
+                }
+            }
+        }
+    }
+}
